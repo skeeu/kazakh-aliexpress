232f8c91c3daee2ff91bf7fe8dabe68ddd74653a
thedakeen <113119945+thedakeen@users.noreply.github.com>
2024-03-04 19:05:49 +0600

    Add files via upload

--- backend/cmd/web/handlers.go
+++ backend/cmd/web/handlers.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"github.com/go-ozzo/ozzo-validation/v4"
+	"kazakh-aliexpress/backend/pkg/models"
 	"net/http"
 )
 
@@ -9,8 +12,37 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Home page"))
 }
 
-func (app *application) signupUser(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Create a new user...")
+func (app *application) signupEmail(w http.ResponseWriter, r *http.Request) {
+	var req struct {
+		Email string `json:"email"`
+	}
+
+	err := json.NewDecoder(r.Body).Decode(&req)
+	if err != nil {
+		app.clientError(w, http.StatusBadRequest)
+		return
+	}
+
+	err = validation.ValidateStruct(&req,
+		validation.Field(&req.Email, validation.Required, validation.Length(5, 100)),
+	)
+
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		json.NewEncoder(w).Encode(map[string]string{"error": err.Error()})
+		return
+	}
+
+	err = app.users.SignUpEmail(req.Email)
+	if err != nil {
+		if err == models.ErrDuplicateEmail {
+			w.WriteHeader(http.StatusConflict)
+			json.NewEncoder(w).Encode(map[string]string{"error": "Email already in use"})
+		} else {
+			app.serverError(w, err)
+		}
+		return
+	}
 }
 
 func (app *application) loginUser(w http.ResponseWriter, r *http.Request) {
